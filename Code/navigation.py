37aa25421f2b2967069ebee03b2a18cc84d3ee06
17231087 <272215951@qq.com>
2020-05-19 23:46:33 +0800

    完善GUI，增加了导航，运动等界面

---
+++ Code/navigation.py
@@ -0,0 +1,7 @@
+class navigation:
+    def set_cur_pos(self):
+        pass
+    def set_tar_pos(self):
+        pass
+    def navigate(self):
+        pass
---
+++ Code/sportEquipment.py
@@ -0,0 +1,18 @@
+class sportEquipment:
+    def towards(self):
+        pass
+    def backward(self):
+        pass
+    def left(self):
+        pass
+    def right(self):
+        pass
+    def end(self):
+        pass
+    def backward(self):
+        pass
+    def move_pause(self):
+        pass
+    def stop_move(self):
+        pass
+
